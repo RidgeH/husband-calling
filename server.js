59e8fd44db4477b6fb829ad6a6ac08756c078bf3
Ridge <ridgeh5@berkeley.edu>
2023-09-03 12:37:31 -0700

    Should be able to register contestant now

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
+const Contestant = require('./models:Contestant');
+
+mongoose.connect('mongodb://localhost:27017/husbandCalling', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+})
+
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.log('Failed to connect to MongoDB'))
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
+
+app.post('/contestants', async (req, res) => {
+    try {
+        const contestant = new Contestant(req.body);
+        await contestant.save();
+        res.status(201).send({ message: "Contestant registered!"});
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
 
