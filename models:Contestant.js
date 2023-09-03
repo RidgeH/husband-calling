4b68b881763da54c9d6adf701746466ce7dc0871
Ridge <ridgeh5@berkeley.edu>
2023-09-03 13:22:33 -0700

    Added error messages

--- models:Contestant.js
+++ models:Contestant.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const contestantSchema = new mongoose.Schema({
-    contestantName: { type: String, required: true, unique: true },
-    husbandName: {type: String, required: true, unique: true }, 
-    vocalRange: {type: Number, required:true }, 
-    location: { type: Number, required: true },
+    contestantName: { type: String, required: [true, "You are missing the contestant's name!"], unique: true },
+    husbandName: {type: String, required: [true, "You are missing the contestant's husband's name!"], unique: true }, 
+    vocalRange: {type: Number, required:[true, "You are missing the contestant's vocal range!"] }, 
+    location: { type: Number, required: [true, "You are missing the husband's location from the contestant zone!"] },
 });
 
 module.exports = mongoose.model('Contestant', contestantSchema);
