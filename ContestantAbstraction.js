1a8470ab17e69778893ed198272d56fd74817c9a
Ridge <ridgeh5@berkeley.edu>
2023-09-04 00:07:24 -0700

    Improved clarity

--- ContestantAbstraction.js
+++ ContestantAbstraction.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
 
 const contestantSchema = new mongoose.Schema({
-    contestantName: { type: String, required: [true, "You are missing the contestant's name!"], unique: true },
-    husbandName: {type: String, required: [true, "You are missing the contestant's husband's name!"], unique: true }, 
-    vocalRange: {type: Number, required:[true, "You are missing the contestant's vocal range!"] }, 
-    location: { type: Number, required: [true, "You are missing the husband's location from the contestant zone!"] },
+    contestantName: { 
+        type: String, 
+        required: [true, "You are missing the contestant's name!"], 
+        unique: true 
+    },
+    husbandName: {
+        type: String, 
+        required: [true, "You are missing the contestant's husband's name!"], 
+        unique: true 
+    }, 
+    vocalRange: {
+        type: Number, 
+        required:[true, "You are missing the contestant's vocal range!"] 
+    }, 
+    location: { 
+        type: Number, 
+        required: [true, "You are missing the husband's location from the contestant zone!"] 
+    },
 });
 
 module.exports = mongoose.model('Contestant', contestantSchema);
